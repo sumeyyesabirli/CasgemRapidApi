e9bd6f8bd3e3f9cbbab117efa3e9b887146b473c
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-20 14:26:44 +0300

    güncellemeler

--- CasgemRapidApi/Controllers/ExchangeController1.cs
+++ CasgemRapidApi/Controllers/ExchangeController1.cs
@@ -29,5 +29,29 @@ namespace CasgemRapidApi.Controllers
             }
 
         }
+
+       
+        public async Task<IActionResult> Index2(string a = "TRY") 
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency={a}&locale=en-gb"),
+                Headers =
+            {
+                { "X-RapidAPI-Key", "d1f43f90aamsh85263efd5fc8d16p1624b4jsnc27aa8d24638" },
+                { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+            },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+            }
+        }
     }
 }
