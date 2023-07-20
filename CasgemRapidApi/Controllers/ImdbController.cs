ef5ce0eedddf72fecd498370cc98f72deedc3aef
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-20 12:15:15 +0300

    Imdb 100 Listeleme

---
+++ CasgemRapidApi/Controllers/ImdbController.cs
@@ -0,0 +1,35 @@
+﻿using CasgemRapidApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace CasgemRapidApi.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            //List<ImdMovieListViewModel> model = new List<ImdMovieListViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "f2aca2c6b5msh6e1ed4b2f16a931p170f45jsndf22767a5549" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                 var model= JsonConvert.DeserializeObject<List<ImdMovieListViewModel>>(body);
+                return View(model.ToList());
+            }
+           
+        }
+    }
+}
---
+++ CasgemRapidApi/Models/ImdMovieListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CasgemRapidApi.Models
+{
+    public class ImdMovieListViewModel
+    {            
+       public int rank { get; set; }
+       public string title { get; set; }      
+       public string rating { get; set; }       
+       public int year { get; set; }    
+
+    }
+}
